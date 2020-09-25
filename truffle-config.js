7afac89cd0404b0c1ef18ca35d5241753d0b62d8
Fabio Bonfiglio <fabio.bonfiglio@protonmail.ch>
2020-09-25 17:29:48 +0200

    Adaptation config truffle Il est maintenant possible de lancer les tests sans devoir lancer manuellement une chaîne test locale

--- truffle-config.js
+++ truffle-config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  networks: {
+  /*networks: {
     development: {
-      host: "127.0.0.1",     // Localhost (default: none)
-      port: 8545,            // Standard Ethereum port (default: none)
-      network_id: "*",       // Any network (default: none)
-    },
-  },
+      host: "127.0.0.1", 
+      port: 8545,
+      network_id: "*",
+    }
+  },*/
   mocha: {
     timeout: 100000
   },
@@ -16,7 +16,7 @@ module.exports = {
       settings: {          // See the solidity docs for advice about optimization and evmVersion
         optimizer: {
           enabled: true,
-          runs: 6000
+          runs: 600
         },
         evmVersion: "byzantium"
       }
